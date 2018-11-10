cace5376b10cbc9cf0a54b2ed1393688b6ea8567
Dmitry Legchikov <legchikov@dmitry.by>
2018-11-10 11:52:03 +0300

    add 10 november

---
+++ src/3november.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello 3 November!" << endl;
+//     return 0;
+// }
+
+// Cортировка пузырьком O(n^2)
+// https://ru.wikipedia.org/wiki/Сортировка_пузырьком
+
+
+// Сортировка вставками O(n^2)
+// https://ru.wikipedia.org/wiki/Сортировка_вставками
+
+
+// Сортировка выбором O(n^2)
+// https://ru.wikipedia.org/wiki/Сортировка_выбором
+
+
+// Описание алгоритмов сортировки и сравнение их производительности
+// https://habr.com/post/335920/
